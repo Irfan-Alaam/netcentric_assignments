6bce0270e9b7ad562285a7e84cb387ff2dd8ef14
Irfan <alamandirfan@gmail.com>
2024-11-19 23:08:06 +0545

    all committed

--- Basic_assignment/Program.cs
+++ Basic_assignment/Program.cs
@@ -10,6 +10,20 @@ namespace Basic_assignment
     {
         static void Main(string[] args)
         {
+            //Swapping values of two variables without using the third variable
+            int first_var = 5;
+            int second_var = 10;
+            Console.WriteLine("Before Swapping:");
+            Console.WriteLine("First_var=" + first_var);
+            Console.WriteLine("Second_var=" + second_var);
+            first_var = first_var + second_var;
+            second_var = first_var - second_var;
+            first_var = first_var - second_var;
+            Console.WriteLine("\nAfter Swapping:");
+            Console.WriteLine("First_var=" + first_var);
+            Console.WriteLine("Second_var=" + second_var);
+
+
         }
     }
 }
